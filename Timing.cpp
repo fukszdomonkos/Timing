56fba9f04635e7d077a76021d14a830f4b0c4211
fukszdomonkos <fukszdomonkos@gmail.com>
2015-04-20 13:44:10 +0200

    more comments

--- Timing.cpp
+++ Timing.cpp
@@ -1,101 +1,141 @@
 #include "Arduino.h"
 #include "Timing.h"
 
+//constructor for Timer class
+Timer::Timer(Time time){
+    isOn = false;
+    startMillis = 0;
+    activeTime = time;
+    presetTime = time;
+}
 
-    Timer::Timer(Time time){
-        isOn = false;
-        startMillis = 0;
-        activeTime = time;
-        presetTime = time;
-    }
-    
-    bool Timer::isActive(){ return isOn; }
-    
-    void Timer::start(){
-        start(presetTime);
-    }
-    void Timer::start(Time time){//start can be called with a different time than preset
-        isOn = true;
-        startMillis = millis();
-        activeTime = time;
-    }
-    void Timer::stop(){
-        isOn = false;
-       // startMillis = 0;
-    }
-    void Timer::update(){
-        Time currentMillis = millis();
-        if((isOn == true) && ((currentMillis - startMillis) > activeTime)){
-            stop();
-        } 
-    }
-    Time Timer::remainingTime(){
-        if(isOn==false) return -1;
-        Time currentMillis = millis();
-        return currentMillis - startMillis;
-    }
+//getter for the active flag
+bool Timer::isActive(){
+    return isOn; 
+}
+
+//starts the timer with the time given in the constructor
+void Timer::start(){
+    start(presetTime);
+}
+
+//starts the timer with the time given (doesn't change the preset time)
+void Timer::start(Time time){
+    isOn = true;
+    startMillis = millis();
+    activeTime = time;
+}
+
+//stops the timer
+void Timer::stop(){
+    isOn = false;
+}
+
+//updater function
+//call this at the beginning of the loop function
+//stops the timer if time runs out
+void Timer::update(){
+    Time currentMillis = millis();
+    if((isOn == true) && ((currentMillis - startMillis) > activeTime)){
+        stop();
+    } 
+}
+
+//calculates the remaining time
+Time Timer::remainingTime(){
+    if(isOn==false) return -1;
+    Time currentMillis = millis();
+    return currentMillis - startMillis;
+}
+
+//constructor fot the ChannelTimer class
+//calls superclass' constructor
+//sets channel low
+ChannelTimer::ChannelTimer(int channel, Time time): Timer(time){
+    channelNumber = channel;
+    pinMode(channelNumber, OUTPUT);
+    channelState = LOW;
+    set();
+}
+
+//sets the channel with the current parameters
+void ChannelTimer::set(){
+    digitalWrite(channelNumber, channelState);
+}
+
+//getter for the channel
+int ChannelTimer::getChannel(){ 
+    return channelNumber; 
+}
+
+//starts with the preset time
+void ChannelTimer::start(){
+    start(presetTime);
+}
+
+//starts with a time given
+void ChannelTimer::start(Time time){
+  
+    Timer::start(time);
+    channelState = HIGH;
+    set();
+}
+
+//stops the timer
+void ChannelTimer::stop(){
+    Timer::stop();
+    channelState = LOW;
+    set();
+}
+
+//constructor for the stopwatch class
+StopWatch::StopWatch(){
+    isOn = false;
+    startTime = 0;  
+    state = RESETED;
+}
+
+//gets the state of the stopwatch
+StopWatch::State StopWatch::getState(){
+    return state;
+}
+
+//starts the stopwatch, reseting it
+void StopWatch::start(){//restarts!!!
+    isOn = true;
+    startTime = millis();
+    state = ACTIVE;
+}
+
+//starts if not started, does nothing if on
+void StopWatch::startOrContinue(){//if on does nothing, if off starts
+    if(isOn==true) return;
+    start();
+}
+
+//stops the stopwatch, changes state to stopped
+void StopWatch::stop(){
+    isOn = false;
+    state = STOPPED;
+}
+
+//changes state to reseted
+void StopWatch::reset(){
+    isOn = false;
+    startTime = 0;
+    state = RESETED;
+}
+
+//gets the elapsed time
+Time StopWatch::elapsedTime(){
+    if (state==RESETED) return 0;
+    Time currentTime = millis();
+    return currentTime - startTime;
+}
+
+//checks if the time given if more than the elapsed
+bool StopWatch::isOver(Time limit){
+    if(limit<elapsedTime()) return true; else return false;
+}
 
 
-
-    StopWatch::StopWatch(/*Time limit*/){
-        isOn = false;
-        startTime = 0;  
-        state = RESETED;
-//        limitTime = limit;
-    }
-    StopWatch::State StopWatch::getState(){return state;}
-    void StopWatch::start(){//restarts!!!
-        isOn = true;
-        startTime = millis();
-        state = ACTIVE;
-    }
-    void StopWatch::startOrContinue(){//if on does nothing, if off starts
-        if(isOn==true) return;
-        start();
-    }
-    void StopWatch::stop(){
-        isOn = false;
-        state = STOPPED;
-        //no reset
-    }
-    void StopWatch::reset(){
-        isOn = false;
-        startTime = 0;
-        state = RESETED;
-    }
-    Time StopWatch::elapsedTime(){
-        if (state==RESETED) return 0;
-        Time currentTime = millis();
-        return currentTime - startTime;
-    }
-
-    bool StopWatch::isOver(Time limit){
-        if(limit<elapsedTime()) return true; else return false;
-    }
-
-
-
-    ChannelTimer::ChannelTimer(int channel, Time time): Timer(time){
-        channelNumber = channel;
-        pinMode(channelNumber, OUTPUT);
-        channelState = LOW;
-        set();
-    }
-    void ChannelTimer::set(){
-        digitalWrite(channelNumber, channelState);
-    }
-    int ChannelTimer::getChannel(){ return channelNumber; }
-    void ChannelTimer::start(){
-        start(presetTime);
-    }
-    void ChannelTimer::start(Time time){
-      
-        Timer::start(time);
-        channelState = HIGH;
-        set();
-    }
-    void ChannelTimer::stop(){
-        Timer::stop();
-        channelState = LOW;
-        set();
-    }
--- Timing.h
+++ Timing.h
@@ -7,14 +7,19 @@
 
 #include "Arduino.h"
 
+//defining a Time type for clearer use
 typedef unsigned long Time;
 
+//Timer and ChannelTimer:
+//call update method in the beginning of the loop! else it never stops by itself
+
+//after started it will stop after the time specified
+//you can read its state any time
 class Timer{
 protected:
     bool isOn;
     Time activeTime;
     Time presetTime;
-    
     Time startMillis;
 public:
     Timer(Time time);
@@ -26,27 +31,9 @@ public:
     Time remainingTime();
 };
 
-class StopWatch{
-public: 
-    enum State{RESETED, ACTIVE, STOPPED};
-private:
-    bool isOn;
-    Time startTime;
-    State state;
-public:   
-    
-    StopWatch();
-    State getState();
-    void start();
-    void startOrContinue();
-    void stop();
-    void reset();
-    Time elapsedTime();
-    bool isOver(Time limit);
-};
-
+//extension for the Timer class with a channel (Pin) that changes with the state
 class ChannelTimer : public Timer{
-private:
+protected:
     int channelNumber;
     int channelState;
 public:
@@ -57,5 +44,27 @@ public:
     void start(Time time);
     void stop();
 };
+
+//counts time from the time it got started
+//you can read its state any time
+class StopWatch{
+public: 
+    enum State{RESETED, ACTIVE, STOPPED};
+protected:
+    bool isOn;
+    Time startTime;
+    State state;
+public:   
+    StopWatch();
+    State getState();
+    void start();
+    void startOrContinue();
+    void stop();
+    void reset();
+    Time elapsedTime();
+    bool isOver(Time limit);
+};
+
+
 
 #endif
